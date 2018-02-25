d668d4987cf8171bb092a5b14b1b26f38d1a8dd6
WP <112@df.pl>
2018-02-25 10:09:44 +0100

    mod hello

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -1,10 +1,14 @@
 package hello;
 
+import javax.swing.JOptionPane;
+
 public class Hello {
 
 	public static void main(String[] args) {
 	//	System.out.println("Hello World");
 		System.out.println("Linia druga");
+		
+		JOptionPane.showMessageDialog(null, "test");
 
 	}
 
